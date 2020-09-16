8bce466b7ebf60a070f00f80fed9d632e7f12ecd
PupakornChutuad <37680367+PupakornChutuad@users.noreply.github.com>
2020-09-16 15:06:27 +0700

    Update Quiz2.js

--- Quiz2.js
+++ Quiz2.js
@@ -6,3 +6,8 @@ console.log('2')
 for(let i = 2; i<=20;i+=2){
     console.log(i)
 }
+console.log('3')
+let sortmul= [-1,2,3,56,6,-93,0,4].map(el => el*2)
+console.log(sortmul)
+let sort= [10,13,44,89,0,12,1].filter(el => el%2===0)
+console.log(sort)
