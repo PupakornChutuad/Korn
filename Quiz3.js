d66e79649fe625780df1c9920e9ac05e50889af2
PupakornChutuad <37680367+PupakornChutuad@users.noreply.github.com>
2020-09-16 15:23:21 +0700

    Update Quiz3.js

--- Quiz3.js
+++ Quiz3.js
@@ -3,6 +3,13 @@ if(grade<=50){
     console.log('F')
 }
 
-else if(grade<=60;grade>=51 ) {
+else if(grade<=60 && grade>=51) {
     console.log('D')
+}
+else if(grade<=70 && grade>=61) {
+    console.log('C')
+}else if(grade<=80 && grade>=71) {
+    console.log('B')
+}else if(grade<=100 && grade>=81) {
+    console.log('A')
 }
